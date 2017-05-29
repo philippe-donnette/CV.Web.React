a9e0ab7a747fc5b9100d6fbaeb68afa377896f7f
philippe Do <philippe.donnette@gmail.com>
2017-05-29 14:47:14 +0100

    Added few extra unit tests for SkillModal component

--- test/components/tag-cloud/skill-modal-test.js
+++ test/components/tag-cloud/skill-modal-test.js
@@ -2,17 +2,18 @@ import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import SkillModal from '../../../src/components/tag-cloud/skill-modal';
+import Modal from '../../../src/components/modal/modal';
+import ErrorModal from '../../../src/components/modal/error-modal';
 import sinon from 'sinon';
 
 describe("src/components/tag-cloud/skill-modal.jsx", function() {
   
   let shallowResult, props, tag; 
 
-  tag = { name: 'tag1', id: 1, iconClass: 'ic-1', weight: 1 };
-
   describe('Main Tests', () => {
 
       beforeEach(() => {
+          tag = { name: 'tag1', id: 1, iconClass: 'ic-1', weight: 1 };
           shallowResult = shallow(<SkillModal skill={tag} />);
       });
     
@@ -21,6 +22,26 @@ describe("src/components/tag-cloud/skill-modal.jsx", function() {
 
       it("renders correct component", () => {
           expect(SkillModal.prototype).to.not.be.null;    
+      });
+
+      it("renders a Modal component", () => {
+          expect(Modal.prototype).to.not.be.null;    
+      });
+
+  });
+
+  describe('No Skill Tests', () => {
+
+      beforeEach(() => {
+        tag = null;
+        shallowResult = shallow(<SkillModal skill={tag} />);
+      });
+    
+      afterEach(() => {
+      });
+
+      it("renders an ErrorModal component", () => {
+          expect(ErrorModal.prototype).to.not.be.null;    
       });
 
   });
