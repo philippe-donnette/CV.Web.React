b3b8dc8e76d0189c74ea20dca3a36b8cc96e9ede
philippe Do <philippe.donnette@gmail.com>
2017-06-02 06:13:02 +0100

    Completed StudyItem component

--- src/components/qualifications/study-item.jsx
+++ src/components/qualifications/study-item.jsx
@@ -5,11 +5,11 @@ class StudyItem extends Component {
     render() {
         let image = <i className="fa fa-image dn-fa-10x"></i>;
         if (this.props.study.imageUrl != null) {
-            image = (<img src={'images/qualifications/' + this.props.study.imageUrl} alt={this.props.study.schoolName} />);
+            image = (<img src={'./assets/' + this.props.study.imageUrl} alt={this.props.study.schoolName} />);
         }
         let certificate = null;
         if (this.props.study.degreeFile != null) {
-            certificate = (<a href={'images/qualifications/' + this.props.study.degreeFile} target="_blank" className="btn dn-bt-secondary" role="button"><i className="fa fa-eye"></i> Certificate</a>);
+            certificate = (<a href={'./assets/' + this.props.study.degreeFile} target="_blank" className="btn dn-bt-secondary" role="button"><i className="fa fa-eye"></i> Certificate</a>);
         }
         let website = null;
         if (this.props.study.websiteUrl != null) {
@@ -21,15 +21,13 @@ class StudyItem extends Component {
             <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
                 <div className="thumbnail text-center dn-study">
                     {image}
-                    <hr className="dn-star-light dn-study-star-light" />
+                    <hr className="dn-star-light dn-study-star-light" /> 
                     <div className="caption text-left">
                         <h3 className="dn-study-h3">{this.props.study.subject}</h3>
                         <h4 className="dn-study-h4">{this.props.study.schoolName}</h4>
                         <p>{this.props.study.city}, {this.props.study.country}</p>
                         <p>
-                            { 
-                                // <i className="fa fa-calendar"></i> {moment(this.props.study.startDate, 'MMM yyyy') } - { moment(this.props.study.endDate, 'MMM yyyy') } 
-                            }
+                            <i className="fa fa-calendar"></i> { moment(this.props.study.startDate).format('MMM YYYY') } - { moment(this.props.study.endDate).format('MMM YYYY') } 
                         </p>
                         <hr className="dn-star-light dn-study-star-light" />
                         <p dangerouslySetInnerHTML={htmlDescription}></p>
--- test/components/qualifications/study-item-test.js
+++ test/components/qualifications/study-item-test.js
@@ -11,7 +11,7 @@ describe("src/components/qualifications/study-item.jsx", function() {
   describe('Main Tests', () => {
 
       beforeEach(() => {
-            study = { id: 1, name: 'study-1' };
+            study = { id: 1, name: 'study-1', imageUrl: 'some-image.png', degreeFile: 'degree.pdf', websiteUrl: 'http://somewhere' };
             shallowResult = shallow(<StudyItem study={study} />);
       });
 
@@ -22,6 +22,46 @@ describe("src/components/qualifications/study-item.jsx", function() {
             expect(StudyItem.prototype).to.not.be.null;    
       });
 
+      it("should render image", () => {
+            let images = shallowResult.find('img');
+            expect(images.node.props.src).to.contain(study.imageUrl);    
+      });
+
+      it("should render certificate link", () => {
+            let link = shallowResult.find('a').nodes.filter(x => { return x.props.href.includes(study.degreeFile); });
+            expect(link.length).to.be.equal(1);    
+      });
+
+      it("should render website link", () => {
+            let link = shallowResult.find('a').nodes.filter(x => { return x.props.href.includes(study.websiteUrl); });
+            expect(link.length).to.be.equal(1);    
+      });
+  });
+
+  describe('Tests with null for imageUrl, degreeFile, websiteUrl', () => {
+
+      beforeEach(() => {
+            study = { id: 1, name: 'study-1', imageUrl: null, degreeFile: null, websiteUrl: null };
+            shallowResult = shallow(<StudyItem study={study} />);
+      });
+
+      afterEach(() => {
+      });
+
+      it("should not render image", () => {
+            let images = shallowResult.find('img');
+            expect(images.length).to.be.equal(0);    
+      });
+
+      it("should not render certificate link", () => {
+            let link = shallowResult.find('a').findWhere(x => x.props().href.includes(study.degreeFile));
+            expect(link.length).to.be.equal(0);    
+      });
+
+      it("should not render website link", () => {
+            let link = shallowResult.find('a').findWhere(x => x.props().href.includes(study.websiteUrl));
+            expect(link.length).to.be.equal(0);    
+      });
   });
 
 });
